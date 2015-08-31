590b63a1e4e13c83bd00f3575d8bbe2cb4c72571
Cornelius Baier <cornelius.baier@gmail.com>
2015-08-31 19:23:28 +0200

    improved cipher.c

--- pset2/cipher.c
+++ pset2/cipher.c
@@ -1,5 +1,37 @@
-- check, if we have only one command line argument, which is going to be the cipher
+#include <stdio.h>
+#include <cs50.h>
 
-if argc != 1
-    return 1
-    
+int check_key(int n);
+
+int main(int argc, string argv[])
+
+{
+
+    // check, if we have only one command line argument, which is going to be the cipher
+   if (argc != 2)
+   {
+       printf("Please enter only one command line argument!! \n");
+       return 1;
+   }
+   else
+   {
+       printf("Please enter your cipher text: \n");
+       string text = GetString();
+       printf("thanks for entering %s \n", text);
+   }
+        
+}
+
+int check_key(int n)
+{
+
+    if (n < 1) 
+    {
+       printf("Please enter a positive integer!!\n");
+       return 1;
+    }
+    else
+    {
+        return n;
+    }
+}
